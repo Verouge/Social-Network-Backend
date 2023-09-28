1520ce06b8d652ed964eaae05ce254953d588afc
Verouge <branola1998628@gmail.com>
2023-09-28 19:09:23 +1000

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -40,3 +40,16 @@ router.post("/", async (req, res) => {
     res.status(400).json({ message: err.message });
   }
 });
+
+// PUT to update a user by its _id
+router.put("/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.json(user);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
