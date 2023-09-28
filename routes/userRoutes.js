a5eff88e2b79ee02cc6fd0c0503d2025de5a3ab4
Verouge <branola1998628@gmail.com>
2023-09-28 19:09:01 +1000

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,3 +12,16 @@ router.get("/", async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
+
+// GET a single user by its _id and populated thought and friend data
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+      .populate("thoughts")
+      .populate("friends");
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
