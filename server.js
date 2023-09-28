9e166b90baaef344d2b0a1cd9ec9b0e59cca8184
Verouge <branola1998628@gmail.com>
2023-09-28 19:17:17 +1000

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const routes = require("./routes");
 
 const app = express();
+const PORT = 3000;
+
+// Connect to MongoDB using Mongoose
 mongoose.connect("mongodb://localhost:27017/social_media_db", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useFindAndModify: false, // This option is added to address the deprecation warning for findByIdAndUpdate
 });
 
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(`Error connecting to MongoDB: ${err}`);
+});
+
+// Middleware to parse incoming JSON data
 app.use(express.json());
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+// Use the consolidated routes from the 'routes' directory
+app.use("/api", routes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
