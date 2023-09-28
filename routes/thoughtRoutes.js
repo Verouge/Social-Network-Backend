6219415b68ff411a474c599a4c5ee22542c8dc04
Verouge <branola1998628@gmail.com>
2023-09-28 19:12:00 +1000

    Update thoughtRoutes.js

--- routes/thoughtRoutes.js
+++ routes/thoughtRoutes.js
@@ -3,4 +3,14 @@ const router = express.Router();
 const Thought = require("../models/Thought");
 const User = require("../models/User");
 
+// GET all thoughts
+router.get("/", async (req, res) => {
+  try {
+    const thoughts = await Thought.find();
+    res.json(thoughts);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 module.exports = router;
