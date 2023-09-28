437afc0bd797b1d7c44d774119c5853d29fa3475
Verouge <branola1998628@gmail.com>
2023-09-28 19:09:13 +1000

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -25,3 +25,18 @@ router.get("/:id", async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
+
+// POST a new user
+router.post("/", async (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    email: req.body.email,
+  });
+
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
